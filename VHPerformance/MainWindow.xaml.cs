d8e49ab661ce1b7a7c4c3a6dc824450101ee3add
Arthur Schut <aschutgh@gmail.com>
2019-01-11 13:36:31 +0100

    Added some task code

--- VHPerformance/MainWindow.xaml.cs
+++ VHPerformance/MainWindow.xaml.cs
@@ -48,5 +48,18 @@ namespace VHPerformance
 
             MessageBox.Show(sw.ElapsedMilliseconds.ToString());
         }
+
+        private void BerekenKwadraat_Click(object sender, RoutedEventArgs e)
+        {
+            int getal = int.Parse(txt.Text);
+            var taak = new Task(() =>
+            {
+
+                int antwoord = Square(getal);
+                MessageBox.Show($"Het kwadraat van {getal} is {antwoord}");
+            });
+
+            taak.Start();
+        }
     }
 }
